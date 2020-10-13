af80035bebaad255ff6f782c49a147b1b3d268b1
Sudipta <sudiptabala32@gmail.com>
2020-10-13 10:14:28 +0530

    [Sudipta] Add : Createing method for Tictactoe game board

---
+++ Tictactoe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] board = new char[3, 3];
+            Initialize(board);
+            print(board);
+        }
+        static void Initialize(char[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+        }
+        static void print(char[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                Console.Write("| ");
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(board[row, col]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
